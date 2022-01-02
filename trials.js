7bca22e6e9b1e57e372d1420616008cc815f7115
ibrahimy353 <ihusssein@gmail.com>
2022-01-02 19:03:47 +0300

    add trials.js

--- trials.js
+++ trials.js
@@ -1,30 +1,28 @@
+let button = document.querySelector('.submit-btn');
+let list = document.querySelector('.todo-list');
 
-let button = document.querySelector('.submit-btn'); 
-//select the element with the class 'submit-btn' and set it to local variable 'button'
-
-button.addEventListener('click', () => {
-    //grab the value from the input and assign it to a variable
-       let textInput = document.querySelector('.new-todo-input').value;
-   //create a new list item element and variable
-       let newToDo = document.createElement("li");
-   //use the input text for the new ToDo item text
-       newToDo.innerText = textInput;
-   //add the item to our existing 'list'
-       list.append(newToDo);
-   });
-   
-let thingsToDo = [      // define array of things to do
+let thingsToDo = [
     "walk the dog",
     "mow the front lawn",
     "redecorate the bookshelves",
     "paint the ceiling to look like the sky on tatooine"
-]
-let list = document.querySelector('.todo-list') //select to-do list unorderd list tag
+];
 
-thingsToDo.map( todoItem => {               //iterate over thingsToDo array
-    let newToDo = document.createElement("li");  //create a var equal to a new list item
+button.addEventListener('click', () => {
+    let textInput = document.querySelector('.new-todo-input').value;
+    updateList(textInput);
+});
+
+thingsToDo.map( todoItem => {
+    updateList(todoItem);
+})
+
+//Add an Element
+function updateList(todoItem){
+    //Create Elements
+    let newToDo = document.createElement("li");
     let deleteBtn = document.createElement("button");
-
+  
     //Assign Text To Elementss
     deleteBtn.innerText = "X";
     newToDo.innerText = todoItem;
@@ -33,8 +31,8 @@ thingsToDo.map( todoItem => {               //iterate over thingsToDo array
     deleteBtn.addEventListener('click', () => {
         newToDo.remove();
     })
-
+  
     //Add Elements to The DOM
-    newToDo.innerText = todoItem;    //set the inner text to the current todoItem string
-     list.append(newToDo);           //add the new lisst item to the todo-list
-})
+    newToDo.append(deleteBtn);
+    list.append(newToDo);
+}
