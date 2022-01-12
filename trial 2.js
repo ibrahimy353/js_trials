3dd3edb06f141868096f6406972157ec7e47b06d
ibrahimy353 <ihusssein@gmail.com>
2022-01-12 05:22:25 +0300

    i am commiting multiple files

--- trial 2.js
+++ trial 2.js
@@ -11,7 +11,18 @@
 //const btn = document.querySelector('#btn');
 //btn.onclick = () => alert("Hello World");
 
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', () => {
-  alert("Hello World");
+//const btn = document.querySelector('#btn');
+//btn.addEventListener('click', () => {alert("Hello World");});
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
 });
