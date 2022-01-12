e8364495d16f99cbb7fb3dff47e86cb0b2334b5f
ibrahimy353 <ihusssein@gmail.com>
2022-01-12 04:51:24 +0300

    i am commiting multiple files

--- trial 2.js
+++ trial 2.js
@@ -6,3 +6,12 @@
 //div.innerText ="HEllo wolrd"
 
 //body.append(div)
+
+//console.log(div.textContent
+//const btn = document.querySelector('#btn');
+//btn.onclick = () => alert("Hello World");
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
